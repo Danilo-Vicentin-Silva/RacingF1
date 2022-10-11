6f692fd7bc36b00d12f8c2a79813143f3ffa2468
Danilo <danilovicentindasilva@gmail.com>
2022-10-11 16:11:01 -0300

    Desenvolvimento dos cards interativos

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,28 @@
-const card1 = document.querySelector('.card1')
-
 const mercedesCard = document.querySelector('#mercedesCard')
 const mercedesCard1 = document.querySelector('#mercedesCard1')
 mercedesCard.addEventListener('click', () => mercedesCard1.style.display = 'block')
+const esconderMercedes = document.querySelector('#esconderMercedes')
+esconderMercedes.addEventListener('click', () => esconderCard1(mercedesCard1))
 
-const esconderCard = document.querySelector('.esconderCard')
-esconderCard.addEventListener('click', () => card1.style.display = 'none')
+
+const ferrariCard = document.querySelector('#ferrariCard')
+const ferrariCard1 = document.querySelector('#ferrariCard1')
+ferrariCard.addEventListener('click', () => ferrariCard1.style.display = 'block')
+const esconderFerrari = document.querySelector('#esconderFerrari')
+esconderFerrari.addEventListener('click', () => esconderCard1(ferrariCard1))
+
+const mclarenCard = document.querySelector('#mclarenCard')
+const mclarenCard1 = document.querySelector('#mclarenCard1')
+mclarenCard.addEventListener('click', () => mostrarCard(mclarenCard1))
+const esconderMclaren = document.querySelector('#esconderMclaren')
+esconderMclaren.addEventListener('click', () => esconderCard1(mclarenCard1))
+
+
+function mostrarCard(card) {
+    card.style.display = 'block'
+}
+function esconderCard1(card) {
+    card.style.display = 'none'
+}
+
 
