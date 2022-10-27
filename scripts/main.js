0687c203afaa6a062c657dc66388a9f763b692df
Danilo <danilovicentindasilva@gmail.com>
2022-10-27 14:55:42 -0300

    v2.1.0 (Desenvolvimento versão PC)

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
+const ferrariCard = document.querySelector('#ferrariCard')
+const ferrariCard1 = document.querySelector('#ferrariCard1')
+ferrariCard.addEventListener('click', () => {
+    ferrariCard1.style.display = 'block'
+})
+const esconderFerrari = document.querySelector('#esconderFerrari')
+esconderFerrari.addEventListener('click', () => esconderCard1(ferrariCard1))
+
 const mercedesCard = document.querySelector('#mercedesCard')
 const mercedesCard1 = document.querySelector('#mercedesCard1')
 mercedesCard.addEventListener('click', () => mercedesCard1.style.display = 'block')
 const esconderMercedes = document.querySelector('#esconderMercedes')
 esconderMercedes.addEventListener('click', () => esconderCard1(mercedesCard1))
-
-
-const ferrariCard = document.querySelector('#ferrariCard')
-const ferrariCard1 = document.querySelector('#ferrariCard1')
-ferrariCard.addEventListener('click', () => ferrariCard1.style.display = 'block')
-const esconderFerrari = document.querySelector('#esconderFerrari')
-esconderFerrari.addEventListener('click', () => esconderCard1(ferrariCard1))
 
 const mclarenCard = document.querySelector('#mclarenCard')
 const mclarenCard1 = document.querySelector('#mclarenCard1')
